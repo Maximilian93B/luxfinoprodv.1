6ab1d2e41af8c074abb5a40a2653e3c688c8b5e9
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-08-29 16:26:00 -0300

    Lux Remote Template Complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
       <Navbar />
 
    {/* Hero section */}
-<div className="hero min-h-screen relative">
+  <div className="hero min-h-screen relative">
   {/* Background Video */}
   <video
     autoPlay
